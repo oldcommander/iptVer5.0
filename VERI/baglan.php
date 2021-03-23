23fe3b92edb748336b1bd332a2226eb11814423d
Ahmet Çekin <ahmetcekin2000@gmail.com>
2021-03-23 10:42:14 +0300

    iptVer1.0

--- VERI/baglan.php
+++
@@ -1,7 +0,0 @@
-<?
-
-$con = mysqli_connect('sql113.epizy.com','epiz_28159435','1h7yc9fq','epiz_28159435_veri');
-if (mysqli_connect_errno() > 0) {
-    die("Hata oluştu: ".mysqli_connect_errno() );
-}
-
